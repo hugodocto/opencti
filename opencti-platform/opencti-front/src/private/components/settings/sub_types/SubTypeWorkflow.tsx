5058debf22e5e2ceb4d76ac8e98866217dc6d307
Octave <octave.albert@filigran.io>
2026-05-07 18:45:53 +0200

    [backend/frontend] update pending tracking and display

--- opencti-platform/opencti-front/src/private/components/settings/sub_types/SubTypeWorkflow.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/sub_types/SubTypeWorkflow.tsx
@@ -59,6 +59,14 @@ export const workflowQuery = graphql`
         }
       }
     }
+    organizations(search: "", first: 200) {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
     statusTemplates(search: "") {
       edges {
         node {
--- opencti-platform/opencti-graphql/src/generated/graphql.ts
+++ opencti-platform/opencti-graphql/src/generated/graphql.ts
@@ -37868,6 +37868,7 @@ export type WorkflowPendingTransition = {
   __typename?: 'WorkflowPendingTransition';
   asyncActions: Array<WorkflowPendingAsyncAction>;
   event: Scalars['String']['output'];
+  syncActions?: Maybe<Array<WorkflowActionConfig>>;
   toState: Scalars['String']['output'];
   triggeredAt: Scalars['DateTime']['output'];
 };
@@ -52614,6 +52615,7 @@ export type WorkflowPendingAsyncActionResolvers<ContextType = any, ParentType ex
 export type WorkflowPendingTransitionResolvers<ContextType = any, ParentType extends ResolversParentTypes['WorkflowPendingTransition'] = ResolversParentTypes['WorkflowPendingTransition']> = ResolversObject<{
   asyncActions?: Resolver<Array<ResolversTypes['WorkflowPendingAsyncAction']>, ParentType, ContextType>;
   event?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  syncActions?: Resolver<Maybe<Array<ResolversTypes['WorkflowActionConfig']>>, ParentType, ContextType>;
   toState?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   triggeredAt?: Resolver<ResolversTypes['DateTime'], ParentType, ContextType>;
 }>;
