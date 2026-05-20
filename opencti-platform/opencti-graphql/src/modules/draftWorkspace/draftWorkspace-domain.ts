7ada5b798963cb1b704e71dfa7b5f7c39881b6bd
Octave <octave.albert@filigran.io>
2026-05-20 12:26:02 +0200

    [backend] Refactor draft context utilities and draft workspace creation (#14057)
    to remove before merge as it was cherry picked from PR 14057 (form
    intake)

--- opencti-platform/opencti-graphql/src/modules/draftWorkspace/draftWorkspace-domain.ts
+++ opencti-platform/opencti-graphql/src/modules/draftWorkspace/draftWorkspace-domain.ts
@@ -41,7 +41,7 @@ import { STIX_EXT_OCTI } from '../../types/stix-2-1-extensions';
 import type { BasicStoreCommon, BasicStoreEntity, BasicStoreRelation, StoreEntity } from '../../types/store';
 import type { AuthContext, AuthUser } from '../../types/user';
 import { getUserAccessRight, KNOWLEDGE_KNUPDATE_KNMANAGEAUTHMEMBERS, SYSTEM_USER } from '../../utils/access';
-import { editAuthorizedMembers } from '../../utils/authorizedMembers';
+import { editAuthorizedMembers, sanitizeAuthorizedMembers } from '../../utils/authorizedMembers';
 import { bypassDraftContext, getDraftContext } from '../../utils/draftContext';
 import { addFilter } from '../../utils/filtering/filtering-utils';
 import { now } from '../../utils/format';
@@ -223,7 +223,18 @@ export const addDraftWorkspace = async (context: AuthContext, user: AuthUser, in
     created_at: now(),
     draft_status: DRAFT_STATUS_OPEN,
   };
-  const draftWorkspaceInput = { ...input, ...defaultOps };
+  let authorizedMembers = input.authorized_members;
+  if (authorizedMembers) {
+    const filteredInput = sanitizeAuthorizedMembers(authorizedMembers);
+    authorizedMembers = filteredInput.map(({ id, access_right, groups_restriction_ids }) => {
+      const member = { id, access_right, groups_restriction_ids };
+      if (!groups_restriction_ids || groups_restriction_ids.length === 0) {
+        delete member.groups_restriction_ids;
+      }
+      return member;
+    });
+  }
+  const draftWorkspaceInput = { ...input, authorized_members: authorizedMembers, ...defaultOps };
   const createdDraftWorkspace = await createEntity(context, user, draftWorkspaceInput, ENTITY_TYPE_DRAFT_WORKSPACE);
   if (createdDraftWorkspace && input.entity_id) {
     const contextInDraft = { ...context, draft_context: createdDraftWorkspace.id };
